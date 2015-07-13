0d29a166b15d55871b836442c576e0944cac41c3
Laura Nawrocki <lwn2@aol.com>
2015-07-13 09:41:45 -0400

    added pseudocode for trivia game.

--- trivia.js
+++ trivia.js
@@ -2,7 +2,25 @@
 
 //Trivia Game.
 //Game should be ready and waiting for an input.
-//Wait for the player to hit the "New Game" button
+//Wait for the player to hit the "New Game" button or
+//have the game do a popup alert asking if the player is
+//ready to play the trivia game.
+//After receiving a "yes" response, start the game.
+//Access first question and answers in trivia storage.
+//Ask first trivia question and offer multiple choice answers.
+//Have the player enter a guess.
+//If player chooses an incorrect answer, tell the player and
+//do not give the player any points.
+//If the player guesses correctly, give the player the points.
+//Keep a running total of correct answers.
+//Move on to the next question and answer choices.
+//If the player gets three incorrect answers, end the game.
+//Or if the player answers all the questions correctly, let
+//the player know that they won.
+//Keep track of the score and give the player the correct number
+//answered correctly after the game is over.
+//Ask the player if they want to play again.
+
 
 
 $(document).ready(function(){
